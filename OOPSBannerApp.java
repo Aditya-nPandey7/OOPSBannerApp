ab8b2a60c26d41a905d7a89277819518b02e657e
Aditya-nPandey7 <an0815@srmist.edu.in>
2026-02-16 09:26:52 +0530

    Refactored code to use BannerPatterns class for UC4

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,32 +1,37 @@
 
-public class OOPSBannerApp {
+class BannerPatterns {
+    
 
-    // This is the new method that handles the printing logic
-    public static void printBanner() {
-        // ROW 1
+    public void printOOPS() {
+
         System.out.println("  *** " + "  *** " + " ****** " + "  ***** ");
         
-        // ROW 2
+
         System.out.println(" * * " + " * * " + " * * " + " * ");
         
-        // ROW 3
         System.out.println(" * * " + " * * " + " * * " + " * ");
         
-        // ROW 4
+
         System.out.println(" * * " + " * * " + " ****** " + "  ***** ");
         
-        // ROW 5
+        
         System.out.println(" * * " + " * * " + " * " + "       * ");
         
-        // ROW 6
+       
         System.out.println(" * * " + " * * " + " * " + "       * ");
         
-        // ROW 7
+       
         System.out.println("  *** " + "  *** " + " * " + "  ***** ");
     }
+}
 
-    // Main method now just calls our custom method
+public class OOPSBannerApp {
+
     public static void main(String[] args) {
-        printBanner();
+        
+        BannerPatterns patterns = new BannerPatterns();
+        
+      
+        patterns.printOOPS();
     }
 }
