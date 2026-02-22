b547e4c7df02c8bb641b0925f0018239a87284fa
Aditya-nPandey7 <an0815@srmist.edu.in>
2026-02-22 19:31:14 +0530

    Corrected UC4 to use String Array and For-Loop

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,37 +1,21 @@
 
-class BannerPatterns {
-    
-
-    public void printOOPS() {
-
-        System.out.println("  *** " + "  *** " + " ****** " + "  ***** ");
-        
-
-        System.out.println(" * * " + " * * " + " * * " + " * ");
-        
-        System.out.println(" * * " + " * * " + " * * " + " * ");
-        
-
-        System.out.println(" * * " + " * * " + " ****** " + "  ***** ");
-        
-        
-        System.out.println(" * * " + " * * " + " * " + "       * ");
-        
-       
-        System.out.println(" * * " + " * * " + " * " + "       * ");
-        
-       
-        System.out.println("  *** " + "  *** " + " * " + "  ***** ");
-    }
-}
-
 public class OOPSBannerApp {
-
     public static void main(String[] args) {
-        
-        BannerPatterns patterns = new BannerPatterns();
-        
-      
-        patterns.printOOPS();
+
+        String[] lines = new String[7];
+
+
+        lines[0] = String.join("", "  *** ", "  *** ", " ****** ", "  ***** ");
+        lines[1] = String.join("", " * * ", " * * ", " * * ", " * ");
+        lines[2] = String.join("", " * * ", " * * ", " * * ", " * ");
+        lines[3] = String.join("", " * * ", " * * ", " ****** ", "  ***** ");
+        lines[4] = String.join("", " * * ", " * * ", " * ", "       *");
+        lines[5] = String.join("", " * * ", " * * ", " * ", "       *");
+        lines[6] = String.join("", "  *** ", "  *** ", " * ", "  ***** ");
+
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
     }
 }
