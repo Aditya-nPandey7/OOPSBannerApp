3576c6756479e5d33e002010785e44a91ed5d3a1
Aditya-nPandey7 <an0815@srmist.edu.in>
2026-03-02 08:48:56 +0530

    Added UC5

---
+++ HelloWorld.java
@@ -0,0 +1,28 @@
+/**
+ * OOPSBannerApp - UC2
+ * Displays "OOPS" as a large ASCII banner.
+ */
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        // ROW 1
+        System.out.println("  *** " + "  *** " + " ****** " + "  ***** ");
+        
+        // ROW 2
+        System.out.println(" * * " + " * * " + " * * " + " * ");
+        
+        // ROW 3
+        System.out.println(" * * " + " * * " + " * * " + " * ");
+        
+        // ROW 4
+        System.out.println(" * * " + " * * " + " ****** " + "  ***** ");
+        
+        // ROW 5
+        System.out.println(" * * " + " * * " + " * " + "       * ");
+        
+        // ROW 6
+        System.out.println(" * * " + " * * " + " * " + "       * ");
+        
+        // ROW 7
+        System.out.println("  *** " + "  *** " + " * " + "  ***** ");
+    }
+}
